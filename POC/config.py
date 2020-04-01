10b1945ab2056eb4d07082dc652e90a2a9240824
nilesh1168 <n.suryawanshi1168@gmail.com>
2020-04-02 02:05:07 +0530

    changed URL of DB to access cloud

--- POC/config.py
+++ POC/config.py
@@ -2,8 +2,14 @@ import os
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 class Config():
-    ENTRIES_PER_PAGE = 10
-    SECRET_KEY = "Asecretkey"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:nilesh@8101998@localhost:3306/dummy'
+    """
+    This is our configuration class
+    database url and other things here...
+"""
+    ENTRIES_PER_PAGE = 10 #this is the number of records that will be displayed on single page : eg 10 students on one page
+    SECRET_KEY = "Asecretkey" # this is the applications secret key
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:admin123@prudent.cgpamkh0cvsh.us-west-2.rds.amazonaws.com:3306/dummy' # DB URL //username:password@address:port/dbname this is the format. 
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
-    SERVICE_WORKER_PATH = "/media/nilesh/Work/BE Project(Sponsored)/POC Feedback Form/POC"
+    SERVICE_WORKER_PATH = "/media/nilesh/Work/BE Project(Sponsored)/POC Feedback Form/POC" #this is the service worker by which we get notifications.
+
+
