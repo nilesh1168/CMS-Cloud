8b856ef4dfe8018d6dd581e7553927f3620b2650
Anuja Pawar <anujapawar64@gmail.com>
2020-04-27 15:39:51 +0530

    chart

--- POC/static/js/genReport.js
+++ POC/static/js/genReport.js
@@ -2,9 +2,56 @@
        
        $.ajax({
            type: "GET",
-           url:  Flask.url_for('genReport',{'id':appConfig.session_id}),
+           url:  Flask.url_for('REPORT',{'id':appConfig.session_id}),
            success: function (response) {
-            console.log(appConfig.session_id)
-           }
+                  var chart = new CanvasJS.Chart("chartContainer", {            
+                  title:{
+                    "verticalAlign": "bottom",
+                    "horizontalAlign": "center",
+                    text: "Pop-up Questions Analysis"              
+                  },
+
+                  horizontalAlign: "center",
+                  verticalAlign: "center",
+                  theme:"light2",
+                  dataPointWidth: 20,
+                  data: [  //array of dataSeries     
+                  { //dataSeries - first quarter
+             /*** Change type "column" to "bar", "area", "line" or "pie"***/        
+                   type: "column",
+                   name: "YES",
+                   
+                   showInLegend: true,
+                   dataPoints: [
+                   { label: "Question 1", y: response["Q1"]["YES"] },
+                   { label: "Question 2", y: 69 },
+                   { label: "Question 3", y: 80 },                                    
+                   { label: "Question 4", y: 74 },
+                   { label: "Question 5", y: 64 }
+                   ]
+                 },
+            
+                 { //dataSeries - second quarter
+            
+                  type: "column",
+                  name: "NO", 
+                  showInLegend: true,               
+                  dataPoints: [
+                  { label: "Question 1", y: response["Q1"]["NO"] },
+                  { label: "Question 2", y: 73 },
+                  { label: "Question 3", y: 88 },                                    
+                  { label: "Question 4", y: 77 },
+                  { label: "Question 5", y: 60 }
+                  ]
+                }
+                ],
+             /** Set axisY properties here*/
+                axisY:{
+                  includeZero: false
+                }    
+              });
+            
+            chart.render();
+            }
        });
     });
