46f5fa00d96301e685d8a818c86d6c0a97cb84bb
nilesh1168 <n.suryawanshi1168@gmail.com>
2020-05-19 12:51:46 +0530

    added env vars

--- POC/config.py
+++ POC/config.py
@@ -9,14 +9,14 @@ class Config():
 """
     ENTRIES_PER_PAGE = 12 #this is the number of records that will be displayed on single page : eg 10 students on one page
     SECRET_KEY = "Asecretkey" # this is the applications secret key
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:admin123@prudent.cgpamkh0cvsh.us-west-2.rds.amazonaws.com:3306/dummy' # DB URL //username:password@address:port/dbname this is the format. 
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+os.getenv('RDS_UNAME')+':'+os.getenv('RDS_PWD')+'@'+os.getenv('RDS_URL')+':3306/'+os.getenv('RDS_DB') # DB URL //username:password@address:port/dbname this is the format. 
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
     SERVICE_WORKER_PATH = base_dir #this is the service worker by which we get notifications.
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT =  465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'developernil98@gmail.com'
-    MAIL_PASSWORD = 'dattaraj@898'
+    MAIL_USERNAME = os.getenv('EMAIL')
+    MAIL_PASSWORD = os.getenv('PWD')
     CERT_PATH = base_dir+'/certificate/'
 
 class TestConfig(Config):
