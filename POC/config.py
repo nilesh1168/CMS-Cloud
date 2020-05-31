7ef354e8ff97bd6db64c2fd27ead3fe8de138a75
Anuja Pawar <anujapawar64@gmail.com>
2020-05-31 10:38:54 +0530

    added users management

--- POC/config.py
+++ POC/config.py
@@ -9,13 +9,8 @@ class Config():
 """
     ENTRIES_PER_PAGE = 12 #this is the number of records that will be displayed on single page : eg 10 students on one page
     SECRET_KEY = "Asecretkey" # this is the applications secret key
-<<<<<<< HEAD
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+os.getenv('RDS_UNAME')+':'+os.getenv('RDS_PWD')+'@'+os.getenv('RDS_URL')+':3306/'+os.getenv('RDS_DB') # DB URL //username:password@address:port/dbname this is the format. 
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/dummy' # DB URL //username:password@address:port/dbname this is the format. 
-=======
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+os.getenv('RDS_UNAME')+':'+os.getenv('RDS_PWD')+'@'+os.getenv('RDS_URL')+':3306/'+os.getenv('RDS_DB') # DB URL //username:password@address:port/dbname this is the format. 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/dummy' # DB URL //username:password@address:port/dbname this is the format. 
->>>>>>> 7a8954e41004b681781056842a842a5398a65eb0
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/dummy' # DB URL //username:password@address:port/dbname this is the format. 
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
     SERVICE_WORKER_PATH = base_dir #this is the service worker by which we get notifications.
     MAIL_SERVER = 'smtp.gmail.com'
