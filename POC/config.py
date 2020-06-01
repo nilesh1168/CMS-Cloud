83473d8524a6794b55d5534bf693f54eb75e7c8b
nilesh1168 <n.suryawanshi1168@gmail.com>
2020-06-01 21:28:10 +0530

    c

--- POC/config.py
+++ POC/config.py
@@ -8,9 +8,9 @@ class Config():
 """
     ENTRIES_PER_PAGE = 12 #this is the number of records that will be displayed on single page : eg 10 students on one page
     SECRET_KEY = "Asecretkey" # this is the applications secret key
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:nilesh@8101998@localhost:3306/dummy'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:nilesh@8101998@localhost:3306/dummy'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+os.getenv('RDS_UNAME')+':'+os.getenv('RDS_PWD')+'@'+os.getenv('RDS_URL')+':3306/'+os.getenv('RDS_DB') # DB URL //username:password@address:port/dbname this is the format. 
-    # SQLALCHEMY_TRACK_MODIFICATIONS  = False
+    SQLALCHEMY_TRACK_MODIFICATIONS  = False
     SERVICE_WORKER_PATH = base_dir #this is the service worker by which we get notifications.
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT =  465
